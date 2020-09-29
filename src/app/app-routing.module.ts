59590050c194e10e88cb57c88fa95cb93f9a7fed
Diego Briceño <diegobrice@hotmail.com>
2020-09-29 00:45:05 -0500

    components and swiper library

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ContadorComponent } from './components/contador/contador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'productos', component: ProductsComponent },
+  { path: 'contacto', component: ContactComponent },
+  { path: 'contador', component: ContadorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { ProductComponent } from './components/product.component';
+import { ContadorComponent } from './components/contador/contador.component';
+import { ProductComponent } from './components/product/product.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 import { LOCALE_ID } from '@angular/core';
 import localeEs from '@angular/common/locales/es';
@@ -14,10 +17,27 @@ import { ExponentialPipe } from './pipes/exponential.pipe';
 import { HighlightDirective } from './directives/highlight.directive';
 import { CircularDirective } from './directives/circular.directive';
 import { ExpPipe } from './pipes/exp.pipe';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
 registerLocaleData(localeEs);
 
 @NgModule({
-  declarations: [AppComponent, ContadorComponent, ProductComponent, ExponentialPipe, HighlightDirective, CircularDirective, ExpPipe],
+  declarations: [
+    AppComponent,
+    ContadorComponent,
+    ProductComponent,
+    ExponentialPipe,
+    HighlightDirective,
+    CircularDirective,
+    ExpPipe,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [{ provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent],
